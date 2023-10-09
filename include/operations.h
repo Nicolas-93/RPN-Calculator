6a2f1ac4036029f822107e6dba729e2e983560cc
Nicolas <nicolas__du95@hotmail.fr>
2023-10-09 19:29:02 +0200

    Operator Structure with function pointer

--- include/operations.h
+++ include/operations.h
@@ -17,24 +17,22 @@ typedef enum BinaryOperator {
     EXPONENT,
 } OperatorName;
 
+typedef int (*UnaryFunction)(int);
+typedef int (*BinaryFunction)(int, int);
 
 typedef struct Operator {
     OperatorType type;
     OperatorName name;
     char* symbol;
+    union {
+        // UnaryFunction unary;
+        // BinaryFunction binary;
+        int (*unary)(int a);
+        int (*binary)(int a, int b);
+    } func;
+    
 } Operator;
 
-Operator OPERATORS[] = {
-    {UNARY_OPERATOR, FACTORIAL, "!"},
-    {UNARY_OPERATOR, EXPONENT, "^"},
-    {BINARY_OPERATOR, ADD, "+"},
-    {BINARY_OPERATOR, SUB, "-"},
-    {BINARY_OPERATOR, DIV, "\\"},
-    {BINARY_OPERATOR, MOD, "%"},
-};
-
-typedef int (*UnaryFunction)(int);
-typedef int (*BinaryFunction)(int, int);
 
 int OP_add(int a, int b);
 
@@ -46,10 +44,19 @@ int OP_sub(int a, int b);
 
 int OP_mod(int a, int b);
 
-int OP_exp(int a);
+int OP_exp(int a, int b);
 
 int OP_factorial(int a);
 
 bool is_operator(char* s, Operator* op);
+
+Operator OPERATORS[] = {
+    {UNARY_OPERATOR, FACTORIAL, "!", OP_factorial},
+    {BINARY_OPERATOR, EXPONENT, "^", OP_exp},
+    {BINARY_OPERATOR, ADD, "+", OP_add},
+    {BINARY_OPERATOR, SUB, "-", OP_sub},
+    {BINARY_OPERATOR, DIV, "/", OP_div},
+    {BINARY_OPERATOR, MOD, "%", OP_mod},
+};
 
 #endif
